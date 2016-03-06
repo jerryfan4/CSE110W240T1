5b973e7533572cd0cfd722045dadcbf0e8fe9c10
YeMaple <gmapleleaf@gmail.com>
2016-03-06 00:01:59 -0800

    Set up Espresso test files

---
+++ src/SystemMoniter/app/src/androidTest/java/com/cse110w240t1/systemmoniter/EspressoTest.java
@@ -0,0 +1,38 @@
+package com.cse110w240t1.systemmoniter;
+
+        import android.support.test.InstrumentationRegistry;
+        import android.support.test.espresso.action.ViewActions;
+        import android.support.test.rule.ActivityTestRule;
+        import android.support.test.runner.AndroidJUnit4;
+        import android.test.ActivityInstrumentationTestCase2;
+        import android.test.suitebuilder.annotation.LargeTest;
+
+        import org.junit.After;
+        import org.junit.Before;
+        import org.junit.Rule;
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+
+        import static android.support.test.espresso.Espresso.onView;
+        import static android.support.test.espresso.action.ViewActions.click;
+        import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+        import static android.support.test.espresso.action.ViewActions.typeText;
+        import static android.support.test.espresso.assertion.ViewAssertions.matches;
+        import static android.support.test.espresso.matcher.ViewMatchers.withId;
+        import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class EspressoTest {
+
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void TestSwipe(){
+
+    }
+
+}
